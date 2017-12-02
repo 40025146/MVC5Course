76a6c5e2400c4eb63e4cfbbc789efe108fed5ccf
PanHongMing <40025146@gm.nfu.edu.tw>
2017-12-02 17:18:29 +0800

    新增Test CRUD

---
+++ MVC5Course/Controllers/TestController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Course.Models;
+using System.Data.Entity;
+using System.Net;
+
+namespace MVC5Course.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        FabricsEntities db = new FabricsEntities();
+        public ActionResult Index()
+        {
+            var data = from p in db.Product
+                       select p;
+            return View(data.ToList().Take(10));
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Product data)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Product.Add(data);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(data);
+        }
+        public ActionResult Edit(int id)
+        {
+            var item=db.Product.Find(id);
+            
+            return View(item);
+        }
+        [HttpPost]
+        public ActionResult Edit(Product data)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(data).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(data);
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            Product product = db.Product.Find(id);
+            if (product== null)
+            {
+                return RedirectToAction("Index");
+            }
+            db.Product.Remove(product);
+            db.SaveChanges();
+            
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(int id)
+        {
+            var item = db.Product.Find(id);
+
+            return View(item);
+        }
+
+    }
+}
