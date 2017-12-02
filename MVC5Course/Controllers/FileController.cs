2f11e0cce944ee8f49885ef1212244579e85a7c8
PanHongMing <40025146@gm.nfu.edu.tw>
2017-12-03 00:19:22 +0800

    檔案上傳

---
+++ MVC5Course/Controllers/FileController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class FileController : Controller
+    {
+        // GET: File
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            if (file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/FileUploads"), fileName);
+
+                file.SaveAs(path);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
