295c0975ca1635993d76cac178d4ac236f0c2be7
PanHongMing <40025146@gm.nfu.edu.tw>
2017-12-02 13:30:05 +0800

    修改modal 的stock 小數點

--- MVC5Course/Models/Product.cs
+++ MVC5Course/Models/Product.cs
@@ -25,9 +25,11 @@ namespace MVC5Course.Models
         [Required(ErrorMessage ="請輸入產品名稱")]
         public string ProductName { get; set; }
         [Required(ErrorMessage = "請輸入產品價格")]
+        [Range(0,999,ErrorMessage ="商品金額必須0~999")]
         public Nullable<decimal> Price { get; set; }
 
         public Nullable<bool> Active { get; set; }
+        [DisplayFormat(DataFormatString ="{0:#}")]
         public Nullable<decimal> Stock { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
